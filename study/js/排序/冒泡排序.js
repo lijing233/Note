fe6da30fd1551ac06aaf33748ecd6350082d92f9
lijing7 <lijing7@gomefinance.com.cn>
2019-05-28 10:54:15 +0800

    添加vue源码部分；添加排序算法部分

---
+++ study/js/排序/冒泡排序.js
@@ -0,0 +1,14 @@
+// 冒泡排序 O(n2)
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i=0; i<len; i++) {
+    for (let j=0; j<len-i-1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
